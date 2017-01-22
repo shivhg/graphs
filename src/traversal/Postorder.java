4004b5fc0f4c92823356a75e6638943e5ed1aca7
shivhg <shivhg@gmail.com>
2017-01-22 23:28:50 +0530

    Post order

---
+++ src/traversal/Postorder.java
@@ -0,0 +1,62 @@
+package traversal;
+
+import java.util.Stack;
+
+/**
+ * Created by hgshivakumar on 22/01/17.
+ */
+public class Postorder {
+    public static void main(String [] args) {
+        Node n = new NodeInitializer().create();
+        recursivePostOrder(n);
+        System.out.print("\n");
+        iterativePostOrder(n);
+    }
+
+    public static void recursivePostOrder(Node root) {
+        if(root == null) {
+            return;
+        }
+        recursivePostOrder(root.left);
+        recursivePostOrder(root.right);
+        System.out.print(root.data);
+    }
+
+    public static void iterativePostOrder(Node root) {
+        Stack<Node> s = new Stack();
+        s.push(root);
+        Node cur = root;
+        Node prev = null;
+
+        while(true) {
+            if (s.empty()) {
+                break;
+            }
+            cur = s.pop();
+            if(cur.right == prev) {
+                System.out.print(cur.data);
+                prev = cur;
+                continue;
+            }
+
+            if(cur.left == prev) {
+                s.push(cur);
+                s.push(cur.right);
+                prev = null;
+                continue;
+            }
+
+            if(cur.left != null) {
+                s.push(cur);
+                s.push(cur.left);
+                prev = null;
+            } else {
+                System.out.print(cur.data);
+                if(cur.right != null) {
+                    s.push(cur.right);
+                }
+                prev = cur;
+            }
+        }
+    }
+}
